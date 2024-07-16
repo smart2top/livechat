db55746420530e12970e2f7d0242b8b143471bd4
smart2top <smart2top.digital@gmail.com>
2024-07-16 10:19:28 -0300

    ajuste token

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,7 +1,7 @@
 export default {
-  secret: process.env.JWT_SECRET || "mad4srsZIISQ0G1MJPoQIeq3PVf25EaR",
+  secret: process.env.JWT_SECRET || "b1c421ee38a557dd2eeb777b9c627a14",
   expiresIn: "3d",
   refreshSecret:
-    process.env.JWT_REFRESH_SECRET || "itI2OQbF3TMy45kIlU7nnhXeIcXugtu9",
+    process.env.JWT_REFRESH_SECRET || "6ac022c53be4cecbf497f60c864e8a62",
   refreshExpiresIn: "7d"
 };
